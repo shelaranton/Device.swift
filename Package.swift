5755e40c7de835938c6863abdc4553108e069e6f
Anton Shelar <shelaranton@gmail.com>
2021-01-11 15:32:29 +0700

    fixed tests

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Device",
+    name: "Device.swift",
     platforms: [
         .iOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Device.swift",
-            targets: ["Device.swift"]),
+            name: "Device",
+            targets: ["Device"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,8 +20,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Device.swift",
-            dependencies: []),
+        .target(name: "Device", dependencies: []),
+        .testTarget(name: "Tests", dependencies: ["Device"]),
     ]
 )
